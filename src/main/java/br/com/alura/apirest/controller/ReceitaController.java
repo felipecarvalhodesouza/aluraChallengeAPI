d80c4672d4ec770ad78ced1dae530a65823b180a
felipecarvalhodesouza <desouzafelipecarvalho@gmail.com>
2022-06-09 15:29:00 -0300

    Removendo uma receita

--- src/main/java/br/com/alura/apirest/controller/ReceitaController.java
+++ src/main/java/br/com/alura/apirest/controller/ReceitaController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -79,6 +80,18 @@ public class ReceitaController {
 			return ResponseEntity.ok(new ReceitaDTO(receita));
 		}
 		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+		Optional<Receita> optional = receitaRepository.findById(id);
+		if (optional.isPresent()) {
+			receitaRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
+
 		return ResponseEntity.notFound().build();
 	}
 }
