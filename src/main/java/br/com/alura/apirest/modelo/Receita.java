ccb5b1a2602fd27f2dd38a64d624a5f75e1a8ae1
felipecarvalhodesouza <desouzafelipecarvalho@gmail.com>
2022-06-09 11:36:24 -0300

    Modelagem de entidades Receita e Despesa

---
+++ src/main/java/br/com/alura/apirest/modelo/Receita.java
@@ -0,0 +1,53 @@
+package br.com.alura.apirest.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Receita {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String descricao;
+	private BigDecimal valor;
+	private LocalDate data;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+}
